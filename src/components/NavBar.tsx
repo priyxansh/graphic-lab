b9951f22aa2acc9f0913e57cd3ff0f48a97cb385
Priyansh Rai <priyanshrai16@gmail.com>
2024-02-20 17:21:12 +0530

    Refactor NavBar component to include UserButton

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
+
 type NavBarProps = {};
 
 const NavBar = ({}: NavBarProps) => {
-    return(
-        <div>NavBar</div>
-    )
-}
+  return (
+    <div className="flex items-center">
+      <div className="hidden lg:flex">
+        <span>Search</span>
+      </div>
+      <div className="ml-auto">
+        <UserButton />
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
