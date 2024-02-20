d75417f426beb0b24ae1156e3d6615afb84f6e5e
Priyansh Rai <priyanshrai16@gmail.com>
2024-02-20 20:52:49 +0530

    Add custom hook for api mutations and add toasts on board creation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "@/providers/Provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider>{children}</Provider>
+        <Provider>
+          {children}
+          <Toaster />
+        </Provider>
       </body>
     </html>
   );
--- src/components/dashboard/NoBoards.tsx
+++ src/components/dashboard/NoBoards.tsx
@@ -1,8 +1,30 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useApiMutation } from "@/hooks/useApiMutation";
+import { useOrganization } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { api } from "../../../convex/_generated/api";
+import { toast } from "sonner";
 
 const NoBoards = () => {
+  const router = useRouter();
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created");
+        router.push(`/board/${id}`);
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <h2 className="text-2xl font-semibold mt-6">Create your first board!</h2>
@@ -10,7 +32,9 @@ const NoBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size="lg">Create board</Button>
+        <Button disabled={pending} size="lg" onClick={onClick}>
+          Create board
+        </Button>
       </div>
     </div>
   );
