09781cff7c1e3772cc03743f588a54f9edf07d57
Paras <parasmahla80@gmail.com>
2024-04-11 16:58:34 +0530

    Changing Loading skeleton

--- src/components/board/Info.tsx
+++ src/components/board/Info.tsx
@@ -7,8 +7,7 @@ export const Info = () => {
     </div>
   );
 }
-
-Info.Skeleton = function InfoSkeleton() {
+export const  InfoSkeleton = () => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-96"/>
        
--- src/components/board/Loading.tsx
+++ src/components/board/Loading.tsx
@@ -1,8 +1,8 @@
 import {  Loader } from "lucide-react";
 
-import { Info } from "./Info";
-import { Participants } from "./Participants";
-import { Toolbar } from "./Toolbar";
+import { InfoSkeleton } from "./Info";
+import { ParticipantsSkeleton } from "./Participants";
+import { ToolbarSkeleton } from "./Toolbar";
 
 export const Loading = () => {
 
@@ -10,9 +10,9 @@ export const Loading = () => {
         <main className="h-full w-full relative bg-neutral-100  touch-none flex justify-center items-center">
             {/* <Loader className="h-6 w-6 text-muted-foreground animate-spin " /> */}
 
-            <Info.Skeleton />
-            <Participants.Skeleton />
-            <Toolbar.Skeleton />
+            <InfoSkeleton />
+            <ParticipantsSkeleton />
+            <ToolbarSkeleton />
         </main>
     )
 }
