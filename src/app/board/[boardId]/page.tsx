e7c1e0df684ee5478a5dc04215afea29d0734a09
Priyansh Rai <priyanshrai16@gmail.com>
2024-04-19 15:35:55 +0530

    Update convex version to 1.11.1 and improve loading screen

--- src/app/board/[boardId]/page.tsx
+++ src/app/board/[boardId]/page.tsx
@@ -1,20 +1,24 @@
 import { Canvas } from "@/components/board/Canvas";
 import { Loading } from "@/components/board/Loading";
 import { Room } from "@/components/global/Room";
+import Spinner from "@/components/global/Spinner";
 interface BoardIdPageProps {
   params: {
     boardId: string;
   };
 }
 
-const BoardIdPage = ({
-  params,
-}: BoardIdPageProps) => {
-
-
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
   return (
-    <Room roomId={params.boardId} fallback={<div>Loading...</div>}>
-    <Canvas boardId={params.boardId} />
+    <Room
+      roomId={params.boardId}
+      fallback={
+        <div className="h-full grid place-items-center">
+          <Spinner />
+        </div>
+      }
+    >
+      <Canvas boardId={params.boardId} />
     </Room>
   );
 };
--- src/components/board/Loading.tsx
+++ src/components/board/Loading.tsx
@@ -1,18 +1,17 @@
-import {  Loader } from "lucide-react";
+import { Loader } from "lucide-react";
 
 import { InfoSkeleton } from "./Info";
 import { ParticipantsSkeleton } from "./Participants";
 import { ToolbarSkeleton } from "./Toolbar";
 
 export const Loading = () => {
+  return (
+    <main className="h-full w-full relative bg-neutral-100  touch-none flex justify-center items-center">
+      {/* <Loader className="h-6 w-6 text-muted-foreground animate-spin " /> */}
 
-    return (
-        <main className="h-full w-full relative bg-neutral-100  touch-none flex justify-center items-center">
-            {/* <Loader className="h-6 w-6 text-muted-foreground animate-spin " /> */}
-
-            <InfoSkeleton />
-            <ParticipantsSkeleton />
-            <ToolbarSkeleton />
-        </main>
-    )
-}
+      <InfoSkeleton />
+      <ParticipantsSkeleton />
+      <ToolbarSkeleton />
+    </main>
+  );
+};
